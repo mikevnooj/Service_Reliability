ef974b570e3b143a1594e170fab249ac50965e4f
Mike Nugent, Jr <michaelvnugent@gmail.com>
2020-09-03 15:39:24 -0400

    add generate report scripts
    all working as of 9/3

---
+++ src/r/Daily_Service_Reliability_report_execute.R
@@ -0,0 +1,9 @@
+# this renders the rmarkdown file for the Service_Reliability report.
+# This is the script that will be run in the windows tasks scheduler.
+
+Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc")
+
+rmarkdown::render(input = "C:/Users/Michael.Nugent/Documents/Projects/R_Projects/nooj/Reporting/Daily/Service_Reliability/src/r/Service_Reliability.Rmd"
+                  ,output_file = paste0(format(Sys.Date(), "%Y%m%d"), " Service Reliability Report",'.html')
+                  ,output_dir = "C:/Users/Michael.Nugent/Documents/Projects/R_Projects/nooj/Reporting/Daily/Service_Reliability/output/html") 
+
