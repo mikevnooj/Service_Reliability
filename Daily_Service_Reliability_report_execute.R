48d9cff01477b90f3b91c77a2135283df9b8b313
Mike Nugent, Jr <michaelvnugent@gmail.com>
2020-09-03 14:46:44 -0400

    rm old file

--- Daily_Service_Reliability_report_execute.R
+++
@@ -1,5 +0,0 @@
-# this renders the rmarkdown file for the Service_Reliability report.
-# This is the script that will be run in the windows tasks scheduler.
-
-rmarkdown::render(input = 'c:/users/scarter/downloads/R_Projects/OTP/Service_Reliability.Rmd', 
-                  output_file = paste0(format(Sys.Date(), "%Y%m%d"), " Service Reliability Report",'.html')) 
