328ead391758f71d7bbebc333e0a41ea162492da
Mike Nugent, Jr <michaelvnugent@gmail.com>
2020-09-08 13:09:30 -0400

    add file to trans man, add email addresses

--- src/r/Daily_Service_Reliability_report_execute.R
+++ src/r/Daily_Service_Reliability_report_execute.R
@@ -7,3 +7,13 @@ rmarkdown::render(input = "C:/Users/Michael.Nugent/Documents/Projects/R_Projects
                   ,output_file = paste0(format(Sys.Date(), "%Y%m%d"), " Service Reliability Report",'.html')
                   ,output_dir = "C:/Users/Michael.Nugent/Documents/Projects/R_Projects/nooj/Reporting/Daily/Service_Reliability/output/html") 
 
+file.copy(
+  from = paste0(
+    "C:/Users/Michael.Nugent/Documents/Projects/R_Projects/nooj/Reporting/Daily/Service_Reliability/output/html/"
+    ,paste0(
+      format(Sys.Date(), "%Y%m%d")
+      , " Service Reliability Report",'.html'
+    )#end paste0
+  )#end paste0
+  ,to = "//iptc-prtsvr1/shared/Transportation Management/Service Reliability Reports"
+)
